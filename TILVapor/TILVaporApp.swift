574e69ad64d0181f0cb2829f180aeb798180d9ec
DevOpsThinh <nguyentruongthinhvn2020@gmail.com>
2022-07-11 11:46:56 +0700

    Initial Commit

---
+++ TILVapor/TILVaporApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILVaporApp.swift
+//  TILVapor
+//
+//  Created by Nguyễn Trường Thịnh on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TILVaporApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
